eb7170bfb977d1cba9295569d5c712b6b46c5b5d
bendoane <ben.doane@gmail.com>
2015-10-28 16:13:16 -0400

    To begin

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,16 @@
 class WelcomeController < ApplicationController
+
+  def index
+    @students = [
+      "Matt",
+      "Ben Doane",
+      "Ben Barnett",
+      "Angie",
+      "Lauren",
+      "Shirley",
+      "Anna",
+      "Michelle",
+    ]
+  end
+
 end
