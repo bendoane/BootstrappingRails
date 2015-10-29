dbd689063c783c9c0ccfbc700dfa0293cc7fb010
bendoane <ben.doane@gmail.com>
2015-10-29 14:28:35 -0400

    Still getting used to this wizardry

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,16 +1,10 @@
 class WelcomeController < ApplicationController
 
   def index
-    @students = [
-      "Matt",
-      "Ben Doane",
-      "Ben Barnett",
-      "Angie",
-      "Lauren",
-      "Shirley",
-      "Anna",
-      "Michelle",
-    ]
+    @students = ["Matt","Ben Doane","Ben Barnett","Angie","Lauren","Shirley","Anna","Michelle"]
   end
+
+
+
 
 end
